8c9fa5d01adf47c47d142e2c0343edc10f13237f
3xcellentStudent <relax_man2000@ukr.net>
2023-10-09 19:50:09 +0300

    Created hardcode in BuyButton component at SectionTitle into productPage /src/components/productPage/SectionTitle/parts/BuyButton. Function call API endpoint in NodeJS server localhost:5000/create-payment and get response. Then I have to put it in redux store and redirecting to PayPal page that client be able to payment monye for product.

--- src/components/productPage/SectionTitle/parts/BuyButton/BuyButton.tsx
+++ src/components/productPage/SectionTitle/parts/BuyButton/BuyButton.tsx
@@ -4,6 +4,9 @@ import {usePathname} from 'next/navigation'
 // import {useDispatch, useSelector} from 'react-redux'
 // import {actionGETProductID} from '@/redux/actions/actions'
 
+const clientId = "AVn0ULKoBMaRcvml-G9PxmNuWtPj8bDYJvtbyxHDHjyi6XuJFSYZN6B-B7ZKBEW4n2LlyjiFlwx9cBBt"
+const secretId = "EHsON8KCADYDTAW2B_2F6Yr8K2TfWmF-EBSFigd8l7nypz0Wk5Hzqap_5nJejQkNKoud9jFY8o8M98rx"
+
 export default function BuyButton(){
 
   // const pathname = usePathname()
@@ -12,13 +15,40 @@ export default function BuyButton(){
   async function handleClick(e: any){
     // e.preventDefault()
     // const [, , collection, productID] = pathname.split('/')
-    // dispatch(actionGETProductID({collection, productID}))
+    // dispatch(actionGETPro0ductID({collection, productID}))
+    const url = "http://localhost:5000/create-payment"
+    const data = {
+      intent: 'sale',
+      payer: {
+        payment_method: 'paypal'
+      },
+      redirect_urls: {
+        return_url: 'https://example.com/success',
+        cancel_url: 'https://example.com/cancel'
+      },
+      transactions: [
+        {
+          amount: {
+            total: '10.00',
+            currency: 'USD'
+          }
+        }
+      ]
+    };
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    }).then(res => res.json()).then(res => console.log(res))
   }
 
 
   return(
     <div className="btn__container">
-      <button onClick={e => handleClick(e)} className="btn__buy-button btn_black_hover">Buy Now</button>
+      <button className="btn__buy-button btn_black_hover" 
+      onClick={e => handleClick(e)}>Buy Now</button>
     </div>
   )
 }
